3929b44268625bf611ccdd860d9f99940c60cc91
LAVS21 <villalta2198@gmail.com>
2025-02-25 01:31:08 -0600

    Correccion de errores CRUD de Task completo

--- BackEnd/routes/web.php
+++ BackEnd/routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
+use App\Http\Controllers\TaskController;
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::get('/tasks/{task}', [TaskController::class, 'show']);
+Route::put('/tasks/{task}', [TaskController::class, 'update']);
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+Route::get('/tasks/{task}/weather', [TaskController::class, 'getWeather']);
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -12,6 +21,8 @@ Route::middleware(['auth'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
